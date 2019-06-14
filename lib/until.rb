1dfd43703c27bd9b9e6ae279e272b5bbc27b0059
Nausherwan Khiljee <nausherwan.khiljee@gmail.com>
2019-06-14 22:26:25 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
         levitation_force = 6
-        
+        looping_string = "Wingardium Leviosa"
         until levatation_force == 10
-          puts "Wingardium Leviosa"
+          puts looping_string
           levitation_force +=
         end
 end
